32bbe49d32ff662266bf07293aa50c125b47f06e
Luka Kiria <l.kiria@bog.ge>
2018-10-08 13:55:50 +0400

    hw2

---
+++ HW2/Assets/_Scripts/BallController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+    private Rigidbody _rb;
+    [SerializeField]
+    private float _speeed; private Renderer _rend;
+    // Use this for initialization
+    void Start () {
+        _rb = GetComponent<Rigidbody>();
+        _rend = GetComponent<Renderer>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        _rb.AddForce(new Vector3(x, 0, y) * _speeed);
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Renderer colRend = collision.gameObject.GetComponent<Renderer>();
+        colRend.material.color = _rend.material.color;
+    }
+}
---
+++ HW2/Assets/_Scripts/TileController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileController : MonoBehaviour {
+
+    private Color _color;
+   
+	
+	// Update is called once per frame
+	void Update () {
+        _color = GetComponent<Renderer>().material.color;
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        collision.gameObject.GetComponent<Renderer>().material.color = _color;
+        GetComponent<Renderer>().material.color = _color;
+    }
+
+}
